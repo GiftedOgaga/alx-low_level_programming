5682759ba06c3c8cb9a834e315600d8db71fdfc5
GiftedOgaga <godgiftogaga2016@gmail.com>
2022-04-14 14:21:37 -0700

    The cause of an infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,13 +8,15 @@
 int main(void)
 {
 	int i;
-	
+
 	printf("Infinite loop incoming :(\n");
 	i = 0;
-	while (i < 10)
-	{
-								                putchar(i); /* This part of the code is responsible for the infinite occurrance of the while loop as it fails to increment the input variable i */
-	}
+/**
+ * while (i < 10)
+ * {
+ * * putchar(i);
+ * }
+ */
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
