4340dc803fc53d4c72b33de59a0690cf89f0d762
GiftedOgaga <godgiftogaga2016@gmail.com>
2022-04-17 10:39:41 -0700

    function to print a square with # symbol

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_square - print a square with #
+ * @size: limit
+ * Return: Always 0 (Success)
+ */
+
+void print_square(int size)
+{
+	int i;
+	int k;
+
+	k = 0;
+	if (size > 0)
+	{
+		if (k < size)
+		{
+			for (i = 0; i <= size; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			k += 1;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
