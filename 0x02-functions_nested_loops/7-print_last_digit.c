002b43a4b86c07417f6d5bad0001c9bc8dc6637c
GiftedOgaga <godgiftogaga2016@gmail.com>
2022-04-19 02:43:29 -0700

    a function that prints last digit of an interger

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,12 @@
 
 int print_last_digit(int n)
 {
+	int last_digit = n % 10;
+
 	if (n < 0)
 	{
-		return (-1 * (n % 10));
+		last_digit *= -1;
 	}
-	else
-	{
-		return (n % 10);
-	}
+	_putchar(last_digit + '0');
+	return (last_digit);
 }
