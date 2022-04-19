e855544a0b42d4e7806b720517ed2f398af9e987
GiftedOgaga <godgiftogaga2016@gmail.com>
2022-04-19 01:34:52 -0700

    a function that prints last digit of an interger

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit-prints the last digit
+ * @n:input of function
+ * Return: Always n % 10  (Success)
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
