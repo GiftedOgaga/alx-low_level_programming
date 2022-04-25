681b0ebbec6578578607f48158df31b21b1caa03
GiftedOgaga <godgiftogaga2016@gmail.com>
2022-04-25 08:26:29 -0700

    copy string from source to destination

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,9 +10,11 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	int j;
+
 	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
-		dest[i] = src[j];
+		dest[j] = src[j];
 	}
 	for ( ; j < n; j++)
 	{
