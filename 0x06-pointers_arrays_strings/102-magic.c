9160a232cfddb0b90430f680d83502d38e476bdd
GiftedOgaga <godgiftogaga2016@gmail.com>
2022-04-25 22:31:12 -0700

    magic code

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
-/**
- * main-Entry point
- *
- * Return:0
- */
-
 int main(void)
 {
 int n;
@@ -14,7 +8,6 @@ int *p;
 
 a[2] = 1024;
 p = &n;
-
 /*
  * write your line of code here...
  * Remember:
