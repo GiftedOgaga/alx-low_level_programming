c4e30c9b77bf7a8b1f5f4c8c692b87a9330a2804
GiftedOgaga <godgiftogaga2016@gmail.com>
2022-05-03 15:44:26 -0700

    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

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,14 +12,40 @@ void print_to_98(int n)
 {
 	while (n < 98)
 	{
-		printf("%d, ", n);
+		if (n > 10)
+		{
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar(n + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
 		n += 1;
 	}
 	while (n > 98)
 	{
-		printf("%d, ", n);
+		if (n > 10 && n < 100)
+		{
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		else if (n > 99)
+		{
+			_putchar(n / 100 + '0');
+			_putchar((n % 100) / 10 + '0');
+			_putchar(n % 10 + '0');
+		}
 		n -= 1;
 	}
-	printf("98");
+
+	_putchar('9');
+	_putchar('8');
 	_putchar('\n');
 }
