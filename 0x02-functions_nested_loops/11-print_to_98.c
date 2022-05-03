fb9b3438680efffe2599cab44dec7083a0a3d6b0
GiftedOgaga <godgiftogaga2016@gmail.com>
2022-05-03 16:53:11 -0700

    function that print all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,6 +10,34 @@
 
 void print_to_98(int n)
 {
+	while (n < 98 && n < 0)
+	{
+		if (n > -10)
+		{
+			_putchar('-');
+			_putchar(-1 * n + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		else if (n < -9 && n > -100)
+		{
+			_putchar('-');
+			_putchar(-1 * (n / 10) + '0');
+			_putchar(-1 * (n % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		else if (n < -99)
+		{
+			_putchar('-');
+			_putchar(-1 * (n / 100) + '0');
+			_putchar((-1 * (n % 100)) / 10 + '0');
+			_putchar(-1 * (n % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		n += 1;
+	}
 	while (n < 98 && n > -1)
 	{
 		if (n > 9)
@@ -45,34 +73,6 @@ void print_to_98(int n)
 			_putchar(' ');
 		}
 		n -= 1;
-	}
-	while (n < 98 && n < 0)
-	{
-		if (n > -10)
-		{
-			_putchar('-');
-			_putchar(-1 * n + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-		else if (n < -9 && n > -100)
-		{
-			_putchar('-');
-			_putchar(-1 * (n / 10) + '0');
-			_putchar(-1 * (n % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-		else if (n < -99)
-		{
-			_putchar('-');
-			_putchar(-1 * (n / 100) + '0');
-			_putchar((-1 * (n % 100)) / 10 + '0');
-			_putchar(-1 * (n % 10) + '0');
-			_putchar(',');
-			_putchar(' ');
-		}
-		n += 1;
 	}
 	_putchar('9');
 	_putchar('8');
