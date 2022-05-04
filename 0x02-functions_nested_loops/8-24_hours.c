ecb6b72aab3a1d4eed44c74d1fffefc3413251c8
GiftedOgaga <godgiftogaga2016@gmail.com>
2022-05-03 23:52:21 -0700

    function that prints every minute of the day of jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,23 +8,29 @@
 
 void jack_bauer(void)
 {
-	int i, j, k, l;
+	int i, j;
 
-	for (i = 0; i < 3; i++)
+	for (i = 0; i < 24; i++)
 	{
-		for (j = 0; j < 4; j++)
+		for (j = 0; j < 60; j++)
 		{
-			for (k = 0; k < 6; k++)
+			if ((i < 10) && (j < 10))
 			{
-				for (l = 0; l < 10; l++)
-				{
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(k + '0');
-					_putchar(l + '0');
-					_putchar('\n');
-				}
+				_putchar('0');
+				_putchar(i + '0');
+				_putchar(':');
+				_putchar('0');
+				_putchar(j + '0');
+				_putchar('\n');
+			}
+			if ((i > 9) && (j > 9))
+			{
+				_putchar(i / 10 + '0');
+				_putchar(i % 10 + '0');
+				_putchar(':');
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
+				_putchar('\n');
 			}
 		}
 	}
