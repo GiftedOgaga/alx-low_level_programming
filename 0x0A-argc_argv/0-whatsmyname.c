ede74fd0b554a1fe4ac3d3de40efc92da65590e0
GiftedOgaga <godgiftogaga2016@gmail.com>
2022-05-08 04:28:31 -0700

    A program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main-Entry point
+ * @argc:count the number of parameters
+ * @argv:pointer of array of pointers to strings entring main
+ * Return: 1 if successful
+ */
+
+int main(int argc, char **argv)
+{
+	return (argv[0]);
+}
