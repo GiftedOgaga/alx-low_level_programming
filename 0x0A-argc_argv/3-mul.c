5942c0e79ccb860cc76e4dbb17df3e6b2f4b1aa5
GiftedOgaga <godgiftogaga2016@gmail.com>
2022-05-08 08:08:50 -0700

    program that multiolies to intergers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main-Entry point
+ * @argc:first parameter
+ * @argv:second parameter
+ * Return: 1 on success
+ */
+
+int main(int argc, char **argv)
+{
+	int mul;
+	int a = atoi(argv[1]);
+	int b = atoi(argv[2]);
+
+	mul = a * b;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", mul);
+		return (0);
+	}
+}
