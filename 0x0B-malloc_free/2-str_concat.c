b0624c6845daad150726dbfb508faf520775a32f
GiftedOgaga <godgiftogaga2016@gmail.com>
2022-05-18 08:24:03 -0700

    concatenate two strings into a new memory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,11 +14,7 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k;
 	char *str;
 
-	if (s1 == NULL)
-	{
-		return (0);
-	}
-	if (s2 == NULL)
+	if ((s1 == NULL) || (s2 == NULL))
 	{
 		return (0);
 	}
