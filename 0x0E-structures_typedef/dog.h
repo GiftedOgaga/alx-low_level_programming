e225c7a610e1fcef29d0e9a56c97ace661afbe2c
GiftedOgaga <godgiftogaga2016@gmail.com>
2022-05-28 16:31:11 +0000

    a function that defines a struct called dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,17 @@
 #ifndef DOG_H_INCLUDE_
 #define DOG_H_INCLUDE_
 
+/**
+ *struct dog -fuction that defines a struct dog
+ *@name: first member
+ *@age: second member
+ *@owner: third member
+ */
+
 struct dog
 {
 	char *name;
-	float aage;
+	float age;
 	char *owner;
 };
 
