571342a2797f42b1e167c7baa871982e09123730
GiftedOgaga <godgiftogaga2016@gmail.com>
2022-05-29 11:41:04 +0000

    task 2

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "dog.h"
-#include <string.h>
+
 /**
  *init_dog - a function that initializes a fuction dog
  *@d: a pointer to struct dog
@@ -12,7 +12,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	strcpy(d->name, name);
+	d->name = name;
 	d->age = age;
 	d->owner = owner;
 }
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *print_dog - a function that prints dog
+ *@d: a pointer to the struct dog
+ *Return: 0 on success otherewise 1
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name = NULL)
+		{
+			return (nill);
+		}
+		else if (d->age = NULL)
+		{
+			return (nill);
+		}
+		else if (d->owner = NULL)
+		{
+			return (nill);
+		}
+		
